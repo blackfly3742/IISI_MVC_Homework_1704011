3cfe84d9107ea1cec5a244550f4fcd8832903916
1704011 <1704011@1704011NB01>
2017-07-14 17:34:32 +0800

    專案建立

---
+++ EmailValidation/EmailValidation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailValidation.Startup))]
+namespace EmailValidation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
