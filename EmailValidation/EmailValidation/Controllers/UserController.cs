62bb01700d47dd74accc1216b39a425fff8eaae3
1704011 <1704011@1704011NB01>
2017-07-18 00:59:09 +0800

    基本功能完成

---
+++ EmailValidation/EmailValidation/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using EmailValidation.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmailValidation.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel pageData)
+        {
+            if (pageData.Account == "mike@mail.com" && pageData.Password == "tree")
+            {
+                pageData.Message = $"您使用{pageData.Account}登入成功。";
+            }
+            return View(pageData);
+        }
+    }
+}
---
+++ EmailValidation/EmailValidation/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using EmailValidate.ValidateAttribute;
+using EmailValidation.ValidateAttribute;
+
+namespace EmailValidation.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [NoIs("skilltree,demo,twMVC", ErrorMessage = "請使用別的名稱")]
+        //[DataType(DataType.EmailAddress)]
+        [Display(Name = "Account")]
+        [Email]
+        //[RegularExpression(@"\w.+\@\w.+")]
+        //http://demo.tc/Post/688
+        //Session3講義
+        public string Account { get; set; }
+        [StringLength(20, MinimumLength = 4)]
+        public string Password { get; set; }
+        public string Message { get; set; }
+
+    }
+}
